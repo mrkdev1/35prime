71c494a67b7779db88be7e0098d8033f28a6ad1e
Mark Rovetta <mkrnrt@gmail.com>
2018-04-23 21:21:29 -0700

    add source

--- README.md
+++ README.md
@@ -1,22 +1,5 @@
 # 35prime
-transforms a DNA sequence 
+transforms a DNA sequence into its complement, reverse, and reverse complement 
 
-Downloads a fasta file from the BOLD Systems Public API.
+This would allow all four transformations to be searched for
 
-API home
-`http://boldsystems.org/index.php/api_home`
-
-PUBLIC DATA API
-`http://boldsystems.org/index.php/resources/api?type=webservices`
-
-example
-`http://boldsystems.org/index.php/API_Public/sequence?taxon=Chordata&geo=Florida&institutions=Smithsonian%20Institution`
-
-`http://boldsystems.org/index.php/API_Public/sequence?taxon=Chordata&geo=Florida&institutions=Smithsonian%20Institution`
-
-Output file is named: fasta.fas
-source code is named seqtrans.go
-package is 35prime
-exe is 35prime.exe
-
-url := "http://boldsystems.org/index.php/API_Public/sequence?bin=BOLD:ACV7374"
--- seqtrans.go
+++ seqtrans.go
@@ -1,33 +1,75 @@
 package main
 
 import (
-     "os"
-     "io"
-     "log"
-     "net/http"
+	"os"
+	"fmt"
+	"strconv"	
+	"strings"
+	"log"
+    "io/ioutil"
+
 )
 
 func main() {
-     // Create output file
-     newFile, err := os.Create("fasta.fas")
-     if err != nil {
-          log.Fatal(err)
-     }
-     defer newFile.Close()
+    file := string(os.Args[1])
+	
+    content, err := ioutil.ReadFile(file)
+    if err != nil {
+        log.Fatal(err)
+    }
+    lines := strings.Split(string(content), "\n")
 
-     // HTTP GET request devdungeon.com 
-     url := "http://boldsystems.org/index.php/API_Public/sequence?bin=BOLD:ACV7374"
-     response, err := http.Get(url)
-     defer response.Body.Close()
+	fmt.Println(lines[0])
+	
+	counta := 0
+	countg := 0
+	countc := 0
+	countt := 0
+	length := 0
 
-     // Write bytes from HTTP response to file.
-     // response.Body satisfies the reader interface.
-     // newFile satisfies the writer interface.
-     // That allows us to use io.Copy which accepts
-     // any type that implements reader and writer interface
-     numBytesWritten, err := io.Copy(newFile, response.Body)
-     if err != nil {
-          log.Fatal(err)
-     }
-     log.Printf("Downloaded %d byte file.\n", numBytesWritten)
+	for i := 1; i < len(lines); i++ {
+	   counta = counta + strings.Count(lines[i], "A")
+	   countg = countg + strings.Count(lines[i], "G")
+	   countc = countc + strings.Count(lines[i], "C")
+	   countt = countt + strings.Count(lines[i], "T")
+	}
+	length = counta + countg + countc + countt
+
+	fmt.Printf("%v bp\n", length)
+
+	a := 100.0*(float64(counta)/float64(length))
+	g := 100.0*(float64(countg)/float64(length))
+	c := 100.0*(float64(countc)/float64(length))
+	t := 100.0*(float64(countt)/float64(length))	
+	
+	fmt.Printf("A: %.1f%%\n", a)
+	fmt.Printf("G: %.1f%%\n", g)
+	fmt.Printf("C: %.1f%%\n", c)
+	fmt.Printf("T: %.1f%%\n", t)
+	fmt.Printf("A/T: %.2f\n", a/t)
+	fmt.Printf("G/C: %.2f\n", g/c)	
+	fmt.Printf("G+C: %.1f%%\n", g+c)	
+	fmt.Printf("A+T: %.1f%%\n", a+t)
+
+	sa := strconv.FormatFloat(a, 'f', 2, 64)
+	sg := strconv.FormatFloat(g, 'f', 2, 64)
+	sc := strconv.FormatFloat(c, 'f', 2, 64)
+	st := strconv.FormatFloat(t, 'f', 2, 64)
+	sadt := strconv.FormatFloat((a/t), 'f', 2, 64)
+	sgdc := strconv.FormatFloat((g/c), 'f', 2, 64)
+	sgc := strconv.FormatFloat((g+c), 'f', 2, 64)
+	sat := strconv.FormatFloat((a+t), 'f', 2, 64)
+
+	sdesc := lines[0]	
+	
+	title := ("## " + sdesc)
+    bpair := strconv.Itoa(length)	
+	head := ("| " + "A%" + " | " + "G%" +  " | " + "C%" +  " | " + "T%" +  " | " + "A/T" +  " | " + "G/C" +  " | " + "G+C" +  " | " + "A+T" +  " |")
+	bar := ("| " + " --- " + " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " |")
+    row := 	("| " + sa + " | " + sg +  " | " + sc +  " | " + st +  " | " + sadt +  " | " + sgdc +  " | " + sgc +  " | " + sat +  " |")
+	
+    err = ioutil.WriteFile("table.md", []byte(title + "\n\n" + bpair + "bp" + "\n\n" + head + "\n" + bar + "\n" + row + "\n"), 0666)
+    if err != nil {
+        log.Fatal(err)
+    }	
 }
