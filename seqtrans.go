b9b6dd7870f1a2be9fa68e072b759254a07ee60e
Mark Rovetta <mkrnrt@gmail.com>
2018-04-26 22:04:53 -0700

    reverse and complement

--- seqtrans.go
+++ seqtrans.go
@@ -19,17 +19,54 @@ func main() {
     }
     lines := strings.Split(string(content), "\n")
 	
-	seq := ""
+	s := ""
 	
 	for i := 1; i < (len(lines)-1); i++ {
-	   seq = seq + lines[i]
+	   s = s + lines[i]
     }
 	
-	fmt.Println(lines[0] + "\n" + seq)
+	fmt.Println(lines[0] + "\n" + s)
 
-    sr := stringutil.Reverse(seq)
+    sr := stringutil.Reverse(s)
 	
-	fmt.Println(lines[0] + " (reversed)\n" + sr)	
+	fmt.Println(lines[0] + " (reversed)\n" + sr)
+
+	src := []rune(sr)
 	
+    // Loop over range of indexes in the slice.
+    for i := range src {
+	    if sr[i]==65 {
+            src[i] = 84
+	    }
+	    if sr[i]==84 {
+            src[i] = 65
+	    }
+	    if sr[i]==71 {
+            src[i] = 67
+	    }
+	    if sr[i]==67 {
+            src[i] = 71
+	    }		
+    }
+	
+	sc := []rune(s)
+	
+    // Loop over range of indexes in the slice.
+    for i := range src {
+	    if s[i]==65 {
+            sc[i] = 84
+	    }
+	    if s[i]==84 {
+            sc[i] = 65
+	    }
+	    if s[i]==71 {
+            sc[i] = 67
+	    }
+	    if s[i]==67 {
+            sc[i] = 71
+	    }		
+    }
+	fmt.Println(lines[0] + " (complement)\n" + string(sc))		
+	fmt.Println(lines[0] + " (complement reversed)\n" + string(src))		
 }
 
